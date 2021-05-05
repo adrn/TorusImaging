04c2d39909dce1af84f0acf788701e00bb880567
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-05 16:26:43 -0400

    improve function to reformat element colum names to latex labels

--- totoro/abundance_helpers.py
+++ totoro/abundance_helpers.py
@@ -1,16 +1,41 @@
 def elem_to_label(elem, dollar=True):
-    num, den = elem.split("_")
+    """
+    Convert an element abundance ratio column name to a Latex label.
 
-    if num.lower() == "alpha":
-        if den.lower() == "m":
-            label = r"[\alpha / {\rm M}]"
-        elif den.lower() == "fe":
-            label = r"[\alpha / {\rm Fe}]"
+    Parameters
+    ----------
+    elem : str
+        The input column name. This should be a string like 'MG_FE' or 'mg_fe'.
+    dollar : bool, optional
+        Controls whether or not to wrap the output label in inline math dollar
+        signs, $.
 
-    else:
-        label = rf"[{{\rm {num.title()} }} / {{\rm {den.title()} }}]"
+    Returns
+    -------
+    elem_label : str
+        The element abundance ratio reformatted as a column name.
+    """
+
+    try:
+        num, den = elem.split("_")
+    except ValueError as e:
+        raise ValueError(
+            f"Invalid element column name '{elem}' - expected a string like X_Y"
+        ) from e
+
+    labels = []
+    for part in map(str.lower, [num, den]):
+        if len(part) > 2:  # Assume it's a greek letter...
+            label = "\\" + part
+        elif part == "m":
+            label = r"{\rm M}"
+        else:
+            label = r"{\rm " + part.title() + " }"
+        labels.append(label)
+
+    label = f"[{labels[0]} / {labels[1]}]"
 
     if dollar:
-        return "$" + label + "$"
+        return f"${label}$"
     else:
         return label
