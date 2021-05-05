140dbd4a9b7e51028351ad27d25855fec57e5ebe
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-05 16:30:55 -0400

    merge action modules

--- totoro/actions.py
+++ totoro/actions.py
@@ -4,10 +4,15 @@ Sanders & Binney 2014.
 """
 
 # Third-party
+import astropy.coordinates as coord
+import astropy.table as at
 import astropy.units as u
 import numpy as np
-import gala.integrate as gi
 import gala.dynamics as gd
+import gala.integrate as gi
+
+# This project
+from .config import rsun as ro, vcirc as vo
 
 
 def get_o2gf_aaf(potential, w0, N_max=10, dt=1 * u.Myr, n_periods=128):
@@ -46,4 +51,27 @@ def get_o2gf_aaf(potential, w0, N_max=10, dt=1 * u.Myr, n_periods=128):
         aaf = {k: np.full(3, np.nan) * aaf_units[k] for k in aaf_units}
 
     aaf = {k: aaf[k].to(aaf_units[k]) for k in aaf_units.keys()}
+    return aaf
+
+
+def get_staeckel_aaf(galpy_potential, w, delta=None, gala_potential=None):
+    from galpy.actionAngle import actionAngleStaeckel
+
+    if delta is None:
+        if gala_potential is None:
+            raise ValueError(
+                "If deltas not specified, you must specify " "gala_potential."
+            )
+        delta = gd.get_staeckel_fudge_delta(gala_potential, w)
+
+    o = w.to_galpy_orbit(ro, vo)
+    aAS = actionAngleStaeckel(pot=galpy_potential, delta=delta)
+
+    aaf = aAS.actionsFreqsAngles(o)
+    aaf = {
+        "actions": np.squeeze(aaf[:3]) * ro * vo,
+        "freqs": np.squeeze(aaf[3:6]) * vo / ro,
+        "angles": coord.Angle(np.squeeze(aaf[6:]) * u.rad),
+    }
+    aaf = at.Table({k: v.T for k, v in aaf.items()})
     return aaf
