792afc6397c74358087144c74932cf738918eeb7
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-01-18 08:19:07 -0500

    fix dict pop bug

--- empaf/model.py
+++ empaf/model.py
@@ -196,7 +196,7 @@ class VerticalOrbitModel:
         """
         self._validate_state()
         params = self.state.copy()
-        params["ln_Omega"] = jnp.log(self.pop("Omega"))
+        params["ln_Omega"] = jnp.log(self.state.pop("Omega"))
         return params
 
     @partial(jax.jit, static_argnames=["self"])
