85759c3edb5deb3aaf667fe19fbde70baae260e6
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-07-07 06:40:04 -0400

    log simpson integrate

--- empaf/jax_helpers.py
+++ empaf/jax_helpers.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+from jax.scipy.special import logsumexp
 
 __all__ = ["simpson"]
 
@@ -30,3 +31,33 @@ def simpson(y, x):
 
     else:  # even
         return integral + 0.5 * dx * (y[-1] + y[-2])
+
+
+@jax.jit
+def ln_simpson(ln_y, x):
+    """
+    Evaluate the log of the definite integral of a function using Simpson's rule
+
+    Note: x must be a regularly-spaced grid of points!
+    """
+
+    num_points = len(x)
+    if num_points % 2 == 0:
+        n_odd = num_points - 1
+    else:
+        n_odd = num_points
+
+    dx = jnp.diff(x)[0]
+    weights_first = jnp.asarray([1.0])
+    weights_mid = jnp.tile(jnp.asarray([4.0, 2.0]), [(n_odd - 3) // 2])
+    weights_last = jnp.asarray([4.0, 1.0])
+    weights = jnp.concatenate([weights_first, weights_mid, weights_last], axis=0)
+    ln_integral = logsumexp(ln_y[:n_odd] + jnp.log(weights), axis=-1) + jnp.log(dx / 3)
+
+    if n_odd == num_points:  # odd
+        return ln_integral
+
+    else:  # even
+        return jnp.logaddexp(
+            ln_integral, jnp.log(0.5 * dx) + jnp.logaddexp(ln_y[-1], ln_y[-2])
+        )
