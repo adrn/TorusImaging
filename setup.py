0754a168b9bb4a4ebb8ddf78a9cd2dcc16267994
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-07-22 12:29:30 -0400

    rename package dir and cleanup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python
-# Licensed under an MIT license - see LICENSE
-
 import os
+
 from setuptools import setup
+
+VERSION_TEMPLATE = """
+try:
+    from setuptools_scm import get_version
+    __version__ = get_version(root='..', relative_to=__file__)
+except Exception:
+    __version__ = '{version}'
+""".lstrip()
 
 setup(
     use_scm_version={
-        "write_to": os.path.join("totoro", "version.py"),
-        "write_to_template": '__version__ = "{version}"\n',
+        "write_to": os.path.join("torusimaging", "version.py"),
+        "write_to_template": VERSION_TEMPLATE,
     }
 )
