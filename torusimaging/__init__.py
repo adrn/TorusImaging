8ee1670735599b4cb2d49944c136adcd28eff7a2
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-10-10 08:48:01 -0400

    overhaul data class

--- torusimaging/__init__.py
+++ torusimaging/__init__.py
@@ -6,4 +6,4 @@ except ImportError:
 from torusimaging.model import *  # noqa
 
 from . import model_helpers, plot  # noqa
-from .data import OTIData  # noqa
+from .data import *  # noqa
--- torusimaging/data.py
+++ torusimaging/data.py
@@ -1,168 +1,202 @@
 import astropy.units as u
 import numpy as np
-from gala.units import galactic
 from scipy.stats import binned_statistic_2d
 
+__all__ = ["get_binned_counts", "get_binned_label"]
 
-class OTIData:
-    def __init__(self, pos, vel, units=galactic, labels=None, label_errs=None):
-        self.units = units
-        self._label_names = list(labels.keys())
 
-        # For now, we only support 1D pos and vel:
-        self._pos = u.Quantity(pos).decompose(galactic).value
-        self._vel = u.Quantity(vel).decompose(galactic).value
+def _get_bins_tuple(bins, units=None):
+    if isinstance(bins, dict):
+        bins = (bins["pos"], bins["vel"])
 
-        if labels is None:
-            labels = {}
-        self.labels = {k: np.asarray(v) for k, v in labels.items()}
+    if units is not None:
+        bins = [b.decompose(units).value if hasattr(b, "unit") else b for b in bins]
+    else:
+        bins = [b.value if hasattr(b, "unit") else b for b in bins]
 
-        if label_errs is None:
-            label_errs = {}
-        self.label_errs = {k: np.asarray(v) for k, v in label_errs.items()}
+    return bins
 
-        if self._pos.shape != self._vel.shape:
-            raise ValueError("Input position and velocity must have the same shape")
-        elif self._pos.ndim != 1:
-            raise ValueError("Input position and velocity must be 1D arrays")
 
-        for k, v in self.labels.items():
-            if self._pos.shape != v.shape:
-                raise ValueError(
-                    "Input position and velocity must have the same shape as the "
-                    f"input labels: label {k} has shape {v.shape} but expected "
-                    f"{self._pos.shape}"
-                )
+def _get_arr(x, units):
+    if units is not None:
+        return x.decompose(units).value if hasattr(x, "unit") else x
+    else:
+        return x.value if hasattr(x, "unit") else x
 
-            if k in self.label_errs:
-                e = self.label_errs[k]
-                if self._pos.shape != e.shape:
-                    raise ValueError(
-                        "Input label errors must have the same shape as the input "
-                        f"input labels: label err {k} has shape {e.shape} but expected "
-                        f"{v.shape}"
-                    )
 
-    def __getitem__(self, slc):
-        return OTIData(
-            self._pos[slc],
-            self._vel[slc],
-            units=self.units,
-            labels={k: v[slc] for k, v in self.labels.items()},
-            label_errs={k: v[slc] for k, v in self.label_errs.items()},
-        )
+@u.quantity_input
+def get_binned_counts(pos: u.kpc, vel: u.km / u.s, bins, units=None):
+    """
+    Parameters
+    ----------
+    pos : quantity-like
+    vel : quantity-like
+    bins : tuple, dict
+        A specification of the bins. This can either be a tuple, where the order
+        is assumed to be (pos, vel), or a dictionary with keys "pos" and "vel".
+    """
 
-    def __len__(self):
-        return len(self._pos)
+    pos = _get_arr(pos, units)
+    vel = _get_arr(vel, units)
 
-    @property
-    def pos(self):
-        return self._pos * self.units["length"]
+    H, xe, ye = np.histogram2d(
+        pos,
+        vel,
+        bins=_get_bins_tuple(bins, units=units),
+    )
+    xc = 0.5 * (xe[:-1] + xe[1:])
+    yc = 0.5 * (ye[:-1] + ye[1:])
+    xc, yc = np.meshgrid(xc, yc)
 
-    @property
-    def vel(self):
-        return self._vel * self.units["length"] / self.units["time"]
+    return {
+        "pos": xc,
+        "vel": yc,
+        "counts": H.T,
+    }
 
-    def _get_bins_tuple(self, bins):
-        if isinstance(bins, dict):
-            bins = (bins["pos"], bins["vel"])
 
-        bins = [
-            b.decompose(self.units).value if hasattr(b, "unit") else b for b in bins
-        ]
-        return bins
+def _infer_intrinsic_scatter(y, y_err, nan_safe=False):
+    import jax
+    import jax.numpy as jnp
+    import jaxopt
 
-    def get_binned_counts(self, bins):
-        """
-        Parameters
-        ----------
-        bins : tuple, dict
-            A specification of the bins. This can either be a tuple, where the order
-            is assumed to be (pos, vel), or a dictionary with keys "pos" and "vel".
-        """
+    @jax.jit
+    def ln_likelihood(p, y, y_err):
+        V = jnp.exp(2 * p["ln_s"]) + y_err**2
+        return jnp.nansum(-0.5 * ((y - p["mean"]) ** 2 / V + jnp.log(2 * jnp.pi * V)))
 
-        H, xe, ye = np.histogram2d(
-            self._pos,
-            self._vel,
-            bins=self._get_bins_tuple(bins),
-        )
-        xc = 0.5 * (xe[:-1] + xe[1:])
-        yc = 0.5 * (ye[:-1] + ye[1:])
-        xc, yc = np.meshgrid(xc, yc)
+    @jax.jit
+    def neg_ln_likelihood(p, y, y_err):
+        return -ln_likelihood(p, y, y_err)
 
-        return {
-            "pos": xc * self.units["length"],
-            "vel": yc * self.units["length"] / self.units["time"],
-            "counts": H.T,
-        }
+    opt = jaxopt.ScipyMinimize(
+        method="L-BFGS-B", fun=neg_ln_likelihood, tol=1e-10, maxiter=1000
+    )
 
-    def get_binned_label(
-        self,
-        bins,
-        label_name=None,
-    ):
-        """
-        Parameters
-        ----------
-        bins : tuple, dict
-            A specification of the bins. This can either be a tuple, where the order
-            is assumed to be (pos, vel), or a dictionary with keys "pos" and "vel".
-        """
-        if label_name is None and len(self._label_names) == 1:
-            label_name = self._label_names[0]
+    p0 = {"mean": np.nanmean(y), "ln_s": jnp.log(np.nanstd(y))}
+    res = opt.run(p0, y=y, y_err=y_err)
 
-        if label_name not in self.labels:
-            raise KeyError(
-                f"Invalid label name '{label_name}' – expected one of "
-                f"{self._label_names}"
-            )
-
-        bins = self._get_bins_tuple(bins)
-        xc = 0.5 * (bins[0][:-1] + bins[0][1:])
-        yc = 0.5 * (bins[1][:-1] + bins[1][1:])
-        xc, yc = np.meshgrid(xc, yc)
-
-        bdata = {
-            "pos": xc * self.units["length"],
-            "vel": yc * self.units["length"] / self.units["time"],
-        }
-
-        label_data = self.labels[label_name]
-        label_err = self.label_errs.get(label_name)
-
-        if label_err is None:
-            # No label errors provided: uncertainty on the mean is related to the
-            # intrinsic scatter
-            stat_mean = binned_statistic_2d(
-                self._pos, self._vel, label_data, bins=bins, statistic="mean"
-            )
-            mean = stat_mean.statistic
-
-            stat_err = binned_statistic_2d(
-                self._pos,
-                self._vel,
-                label_data,
-                bins=bins,
-                statistic=lambda x: np.nanstd(x) / np.sqrt(len(x)),
-            )
-            err = stat_err.statistic
-
+    s = np.exp(res.params["ln_s"])
+    if not res.state.success:
+        if nan_safe:
+            s = np.nan
         else:
-            # Label errors provided:
-            stat_mean1 = binned_statistic_2d(
-                self._pos,
-                self._vel,
-                label_data / label_err**2,
-                bins=bins,
-                statistic="sum",
+            raise RuntimeError(
+                "Failed to determine error-deconvolved estimate of the intrinsic "
+                "scatter in a phase-space pixel."
             )
-            stat_mean2 = binned_statistic_2d(
-                self._pos, self._vel, 1 / label_err**2, bins=bins, statistic="sum"
+
+    return s
+
+
+@u.quantity_input
+def get_binned_label(
+    pos: u.kpc,
+    vel: u.km / u.s,
+    label,
+    bins,
+    label_err=None,
+    units=None,
+    s=None,
+    s_N_thresh=128,
+):
+    """
+
+    TODO: currently only supports returning the mean and uncertainty on the mean.
+    TODO: how it computed binned label error
+    TODO: s as intrinsic scatter
+
+    Parameters
+    ----------
+    bins : tuple, dict
+        A specification of the bins. This can either be a tuple, where the order
+        is assumed to be (pos, vel), or a dictionary with keys "pos" and "vel".
+    """
+    pre_err_state = np.geterr()
+    np.seterr(divide="ignore", invalid="ignore")
+
+    pos = _get_arr(pos, units)
+    vel = _get_arr(vel, units)
+    bins = _get_bins_tuple(bins, units)
+
+    xc = 0.5 * (bins[0][:-1] + bins[0][1:])
+    yc = 0.5 * (bins[1][:-1] + bins[1][1:])
+    xc, yc = np.meshgrid(xc, yc)
+
+    binned = {
+        "pos": xc,
+        "vel": yc,
+    }
+
+    # For bin numbers and other stuff below:
+    counts_stat = binned_statistic_2d(
+        pos, vel, None, bins=bins, statistic="count", expand_binnumbers=True
+    )
+    counts = counts_stat.statistic
+
+    if label_err is None:
+        # No label errors provided - assume dominated by intrinsic scatter
+        label_err = np.zeros_like(label)
+
+        if s is None:
+            # estimate just doing the stddev of bins with more than N objects
+            std_stat = binned_statistic_2d(
+                pos, vel, label, bins=bins, statistic=np.nanstd
             )
-            mean = stat_mean1.statistic / stat_mean2.statistic
-            err = np.sqrt(1 / stat_mean2.statistic)
+            s = np.nanmean(std_stat.statistic[counts > s_N_thresh])
 
-        bdata[label_name] = mean.T
-        bdata[f"{label_name}_err"] = err.T
+    if s is None:
+        # Label errors providd, but no intrinsic scatter provided - need to estimate
+        # this for bins with many objects
+        high_N_bins = np.stack(np.where(counts > 128)) + 1
+        high_N_bins = high_N_bins[:, np.argsort(counts[np.where(counts > 128)])[::-1]]
+        high_N_bins = high_N_bins[:, np.any(high_N_bins != 1, axis=0)]
 
-        return bdata, label_name
+        # TODO: magic number - this limits to a subset of the 16 most populated bins
+        s_trials = []
+        for bin_idx in high_N_bins.T[:16]:
+            bin_mask = np.all(counts_stat.binnumber == bin_idx[:, None], axis=0)
+            # to get bin location: stat.x_edge[bin_idx[0]], stat.y_edge[bin_idx[1]]
+
+            s_trials.append(
+                _infer_intrinsic_scatter(
+                    label[bin_mask], label_err[bin_mask], nan_safe=True
+                )
+            )
+        s = np.nanmean(s_trials)
+
+        if not np.isfinite(s):
+            raise ValueError("Failed to determine intrinsic scatter from label data")
+
+    if np.all(label_err == 0):
+        # No label errors provided
+        stat_mean = binned_statistic_2d(
+            pos, vel, label, bins=bins, statistic=np.nanmean
+        )
+        mean = stat_mean.statistic
+        mean_err = 0.0
+
+    else:
+        # Compute the mean and the "error on the mean" in each bin:
+        stat_mean1 = binned_statistic_2d(
+            pos,
+            vel,
+            label / label_err**2,
+            bins=bins,
+            statistic="sum",
+        )
+        stat_mean2 = binned_statistic_2d(
+            pos, vel, 1 / label_err**2, bins=bins, statistic="sum"
+        )
+        mean = stat_mean1.statistic / stat_mean2.statistic
+        mean_err = np.sqrt(1 / stat_mean2.statistic)
+
+    binned["counts"] = counts
+    binned["label"] = mean.T
+    binned["label_err"] = np.sqrt(mean_err**2 + s**2 / counts).T
+    binned["label_err"][~np.isfinite(binned["label_err"])] = np.nan
+    binned["s"] = s
+
+    np.seterr(**pre_err_state)
+
+    return binned
